8b4a164f920f4eff03b8eebc2f6231f2550e6263
zhangzh123455 <765749763@qq.com>
2022-05-21 13:32:22 +0800

    speech_recognition_tankwar1.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TankWar',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/zhangzh123455/speech_regression_tankWar',
+    license='',
+    author='zzh',
+    author_email='765749763@qq.com',
+    description='speech_regression_tankwar'
+)
